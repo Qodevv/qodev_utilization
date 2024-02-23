95534408b3ad773036985f45c70bb30fb69322c5
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-02-23 16:13:31 +0800

    Ignored

--- obj/Debug/net8.0/qodev_utilization.AssemblyInfo.cs
+++ obj/Debug/net8.0/qodev_utilization.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("qodev_utilization")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b16337129c40ee168f63af2aee98e5658e37312")]
 [assembly: System.Reflection.AssemblyProductAttribute("qodev_utilization")]
 [assembly: System.Reflection.AssemblyTitleAttribute("qodev_utilization")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
